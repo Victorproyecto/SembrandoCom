03f391476f3b7967ec0c90ccd9fd1c74a2087a29
InesJaeger <inesjaeger32@gmail.com>
2024-11-19 20:18:02 +0100

    Fix index.html

--- index.php
+++ index.php
@@ -4,13 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sembrando Comunidad</title>
-    <link rel="stylesheet" href="../vista/css/styles_oscar.css">
-    <link rel="stylesheet" href="../vista/css/styles header-footer.css">
+    <link rel="stylesheet" href="vista/css/styles_oscar.css">
+    <link rel="stylesheet" href="vista/css/styles header-footer.css">
 </head>
 <body>
     <header class="header-index">
         <div class="logo-index">
-            <img src="img/imagenes/logo.png" alt="Logo Sembrando Comunidad">
+            <img src="vista/img/imagenes/logo.png" alt="Logo Sembrando Comunidad">
         </div>
         <h1>Sembrando Comunidad</h1>
     </header>
@@ -18,10 +18,10 @@
         <div class="hero-section">
             <h1 class="main-title">Volver a las raíces</h1>
             <p class="subtitle">Únete a la transformación de tu ciudad</p>
-            <a href="register.html" class="register-btn">Regístrate</a>
+            <a href="vista/register.html" class="register-btn">Regístrate</a>
             <div class="login-link">
                 <span>¿Ya tienes una cuenta? </span>
-                <a href="login.html">Inicia Sesión</a>
+                <a href="vista/login.html">Inicia Sesión</a>
             </div>
         </div>
     </main>
